7c9743f2233fce4beaaef5819defbe00f5e01f97
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2023-02-09 10:27:42 +0200

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable <Category> categoryList= _context.Categories;
+            return View(categoryList);
+        }
+    }
+}
