e795205f094d5996f793a02c5d694df626db60ff
Nikolay Shtipkov <nikolay.shtipkov@scalefocus.com>
2023-02-21 11:04:40 +0200

    Edit functionallity

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -53,5 +53,52 @@ namespace BulkyBookWeb.Controllers
 
             return View(category);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
+            }
+
+            if (_context.Categories.Any(c => c.Name == category.Name))
+            {
+                ModelState.AddModelError("Name", "Category name already in use.");
+            }
+
+            if (_context.Categories.Any(c => c.DisplayOrder == category.DisplayOrder))
+            {
+                ModelState.AddModelError("DisplayOrder", "Display order already in use");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
     }
 }
